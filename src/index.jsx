25f62bb7e06834c6186f16fe56528e11878054f2
Gur Shalev <shalevgur1@gmail.com>
2024-09-14 17:37:16 +0300

    full base project

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// For running locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch the react project in your browser
